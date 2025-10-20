a613ff3dc92f42c6398c0d825ed4c811966ab2b7
ProfVespertino <ProfVespertino@PROFESOR04.informatica.comercio>
2025-10-20 21:47:10 +0200

    Unidad 3 CSS gods

---
+++ Unidad 3/main.js
@@ -0,0 +1,10 @@
+const EURO_TO_DOLLAR = 1.05;
+const LIBRA_TO_DOLLAR = 1.05;
+
+const btnSubmit = document.getElementsByClassName("inputSubmit")[0];
+
+btnSubmit.addEventListener("click", function juan(event) {
+  const element = document.getElementsByTagName("input")[0];
+
+  event.preventDefault();
+});
