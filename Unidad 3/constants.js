67d8f3cedae32146c4137bd70286d9f6f20823a2
ProfVespertino <ProfVespertino@PROFESOR04.informatica.comercio>
2025-10-22 17:45:35 +0200

    Unidad 3 JS Sadness

---
+++ Unidad 3/constants.js
@@ -0,0 +1,6 @@
+export const EURO_TO_DOLLAR = 1.05;
+export const LIBRA_TO_DOLLAR = 1.3;
+export const DIVISA_LIBRA = "Libras";
+export const DIVISA_EURO = "Euros";
+export const DIVISA_DOLAR = "Dolares";
+export const DIVISA_CAKE = "🎂";
--- Unidad 3/main.js
+++ Unidad 3/main.js
@@ -1,10 +1,50 @@
-const EURO_TO_DOLLAR = 1.05;
-const LIBRA_TO_DOLLAR = 1.05;
+import { cambioDivisa } from "./cambioDivisa.js";
 
 const btnSubmit = document.getElementsByClassName("inputSubmit")[0];
 
-btnSubmit.addEventListener("click", function juan(event) {
-  const element = document.getElementsByTagName("input")[0];
-
+btnSubmit.addEventListener("click", (event) => {
   event.preventDefault();
+  const element = document.getElementsByTagName("input")[0];
+  let valorNumerico = parseInt(element.value);
+
+  const divisaFrom = document.getElementById("inputDivisaFrom");
+  const divisaTo = document.getElementById("inputDivisaTo");
+
+  const divisaValueFrom = divisaFrom[divisaFrom.selectedIndex].value;
+  const divisaValueTo = divisaTo[divisaTo.selectedIndex].value;
+  let cambio = cambioDivisa(divisaValueFrom, divisaValueTo, valorNumerico);
+  addHistoricExchange(
+    valorNumerico,
+    cambio,
+    divisaValueFrom,
+    divisaValueTo,
+    new Date(Date.now())
+  );
+});
+
+function addHistoricExchange(
+  valueOrigin,
+  valueExchanged,
+  divisaFrom,
+  divisaTo,
+  fechaActual
+) {
+  const sectionHistorico = document.getElementById("historicSection");
+  const nuevaLineaHistorico = document.createElement("p");
+  const valorNumericoExchanged = valueExchanged.toString().slice(0, 5);
+  const textHistoricoNuevaLinea = document.createTextNode(
+    `${fechaActual.getDate()}/${fechaActual.getMonth()}/${fechaActual.getFullYear()} 
+    ${fechaActual.getHours()}:${fechaActual.getMinutes()} 
+    Importe ${valueOrigin} ${divisaFrom} - ${valorNumericoExchanged} ${divisaTo}`
+  );
+  nuevaLineaHistorico.appendChild(textHistoricoNuevaLinea);
+  sectionHistorico.appendChild(nuevaLineaHistorico);
+}
+
+document.getElementById("imagenArrow").addEventListener("click", (event) => {
+  const divisaFrom = document.getElementById("inputDivisaFrom");
+  const divisaTo = document.getElementById("inputDivisaTo");
+  const auxiliarSelectedIndexTo = divisaTo.selectedIndex;
+  divisaTo.selectedIndex = divisaFrom.selectedIndex;
+  divisaFrom.selectedIndex = auxiliarSelectedIndexTo;
 });
