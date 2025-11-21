8e9c8f2b92c7c5975fbb8af0dcad644d274fa9f3
rjalamanac <rjalamanac@iescomercio.com>
2025-11-21 17:52:25 +0100

    Dumb btn

---
+++ Unidad 5/MovingThings/main.js
@@ -0,0 +1,10 @@
+const btn = document.getElementById("idBtn");
+btn.addEventListener("mouseover", moveIt);
+
+function moveIt(event) {
+  let randomY = Math.random() * window.screen.height;
+  let randomX = Math.random() * window.screen.width;
+  btn.style.position = "absolute";
+  btn.style.top = Math.floor(randomY) + "px";
+  btn.style.left = Math.floor(randomX) + "px";
+}
