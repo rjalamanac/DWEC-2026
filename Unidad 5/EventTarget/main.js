9d114b808a66bbf737fd810d52a6c72ee83d5368
ProfVespertino <ProfVespertino@PROFESOR04.informatica.comercio>
2025-11-21 20:16:30 +0100

    Unidad 3 JS Sadness

--- Unidad 5/EventTarget/main.js
+++ Unidad 5/EventTarget/main.js
@@ -5,13 +5,14 @@ for (let index = 0; index < Math.random() * 100; index++) {
 function createButton(id) {
   const btn = document.createElement("button");
   btn.textContent = id;
-  btn.jamon = id;
   btn.addEventListener("click", clickMe);
   document.getElementById("idMain").appendChild(btn);
 }
 
 function clickMe(event) {
-  const intValue = parseInt(document.getElementById("idResult").textContent);
+  const valueContador = parseInt(
+    document.getElementById("idResult").textContent
+  );
   document.getElementById("idResult").textContent =
-    intValue + parseInt(event.target.jamon);
+    valueContador + parseInt(event.target.textContent);
 }
